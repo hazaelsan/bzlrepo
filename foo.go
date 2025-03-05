1a8ff52f76fd79cde300cc6268f590ab13d06ab6
Hazael Sanchez <hazaelsan@gmail.com>
2025-03-05 18:23:33 +0000

    Remove googleapis-go
    Only exports a `proto_library` to make troubleshooting easier.

--- foo.go
+++ foo.go
@@ -1,9 +1,5 @@
 // Package bzlrepo is a pretend Go library.
 package bzlrepo
 
-import "github.com/hazaelsan/bzlrepo/proto/v1/foo"
-
 // Foo is some value.
 const Foo string = "Some value"
-
-type _ = foo.Foo

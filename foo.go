b110f44758d680dc3ef8fe5b6e6dfee180592767
Hazael Sanchez <hazaelsan@gmail.com>
2025-03-12 18:44:56 +0000

    Add a dummy non-Bazel dependency

--- foo.go
+++ foo.go
@@ -1,7 +1,10 @@
 // Package bzlrepo is a pretend Go library.
 package bzlrepo
 
-import _ "github.com/hazaelsan/bzlrepo/proto/v1/foo"
+import (
+	_ "github.com/hazaelsan/bzlrepo/proto/v1/foo"
+	_ "github.com/kylelemons/godebug/pretty"
+)
 
 // Foo is some value.
 const Foo string = "Some value"
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hazaelsan/bzlrepo
 
 go 1.22.7
+
+require github.com/kylelemons/godebug v1.1.0
